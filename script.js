926cbd523fcd7088e198ca0fcf2c1a253e96e5f7
Ted Shishkovskiy <tmshishkovskiy@gmail.com>
2022-08-22 20:14:15 -0400

    added all the questions and answers to the quiz via script.js

--- script.js
+++ script.js
@@ -18,3 +18,56 @@ var buttonA = document.getElementById("a");
 var buttonB = document.getElementById("b");
 var buttonC = document.getElementById("c");
 var buttonD = document.getElementById("d");
+
+// Quiz Question object (Code Quiz)
+var quizQuestions = [ {
+    question: "What is most commonly used to add styling to web pages",
+    choiceA: "Python",
+    choiceB: "React.js",
+    choiceC: "CSS",
+    ChoiceD: "HTML",
+    correctAnswer: "c"},
+    {
+    question: "Which HTML attribute would you use to reference an external JavaScript file?",
+    choiceA: "index",
+    choiceB: "src",
+    choiceC: "class",
+    ChoiceD: "src",
+    correctAnswer: "d"},
+    {
+    question: "What part of web application do users interact with?",
+    choiceA: "Middle Tier",
+    choiceB: "Front End",
+    choiceC: "Back End",
+    ChoiceD: "Full Stack",
+    correctAnswer: "b"},
+    {
+    question: "What symbol is used to represent the/and operator in JavaScript?",
+    choiceA: "//",
+    choiceB: "&&",
+    choiceC: "||",
+    ChoiceD: "==",
+    correctAnswer: "b"},
+    {
+    question: "In which HTML element do we put JavaScript?",
+    choiceA: "<script>",
+    choiceB: "<javascript>",
+    choiceC: "<div>",
+    ChoiceD: "<js>",
+    correctAnswer: "a"},
+    {
+    question: "What basic ways cn people become aware of your website?",
+    choiceA: "Url is shared by somebody",
+    choiceB: "The link is attached on another website",
+    choiceC: "Your website is listed in a search engine",
+    ChoiceD: "All of the above",
+    correctAnswer: "d"},
+    {
+    question: "Which of the following is a local operator in JavaScript?",
+    choiceA: "/",
+    choiceB: "|",
+    choiceC: "&&",
+    ChoiceD: "%",
+    correctAnswer: "c"},
+
+];
